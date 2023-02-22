7fc3490f086cf00a129a423a12af6e2e7e98cd77
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-22 13:26:11 -0800

    do it

--- date-time.cs
+++ date-time.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 
 DateTime now;
 DateTime today;
-now = DateTime.Now;
-today = DateTime.Today;
-Console.WriteLine("Now is " + now);
-Console.WriteLine("Today is " + today.ToLongDateString());
+now = DateTime.Now; // current date and time
+today = DateTime.Today; // current date
+DateTime tomorrow = today.AddDays(1);
+Console.WriteLine("Now is " + now); // display current date and time
+Console.WriteLine("Today is " + today); // display current date
+Console.WriteLine("Today is " + today.ToLongDateString()); // display current date in long form
+Console.WriteLine("I will start learning on " + tomorrow.ToShortDateString() + ".");
 Console.ReadLine();
