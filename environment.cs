52a503ee9296c88a39af4719783d8bd065aa279c
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-22 13:15:25 -0800

    written

---
+++ environment.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+Console.WriteLine("Device name: " + Environment.MachineName);
+Console.WriteLine("64-bit system: " + Environment.Is64BitOperatingSystem);
+Console.WriteLine("Username: " + Environment.UserName);
