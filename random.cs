2b12534974109e067adfa107bc4fae6484f19f11
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-22 13:45:38 -0800

    dualdice

--- random.cs
+++ random.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 Random randomNumbers = new Random(); // create random number generator object
 
-// generate 3 random numbers
-int num1 = randomNumbers.Next(1, 6+1);
-int num2 = randomNumbers.Next(1, 6+1);
-int num3 = randomNumbers.Next(1, 6+1);
+// generate first couple
+int num11 = randomNumbers.Next(1, 6+1);
+int num12 = randomNumbers.Next(1, 6+1);
+
+int num21 = randomNumbers.Next(1, 6+1);
+int num22 = randomNumbers.Next(1, 6+1);
+
+int num31 = randomNumbers.Next(1, 6+1);
+int num32 = randomNumbers.Next(1, 6+1);
 
 // Output
-Console.WriteLine("Thrown Numbers: " + num1 + " " + num2 + " " + num3);
+Console.WriteLine("Couple 1: " + num11 + "-" + num12);
+Console.WriteLine("Couple 2: " + num21 + "-" + num22);
+Console.WriteLine("Couple 3: " + num31 + "-" + num32);
