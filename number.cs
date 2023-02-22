5c049c719efe92605912ab24e5189247a904f57d
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-22 11:59:37 -0800

    added using, removed other

--- number.cs
+++ number.cs
@@ -1,8 +1,8 @@
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine(37);
-        Console.ReadLine();
-    }
-}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+Console.WriteLine(37);
+Console.ReadLine();
