f64c10a23f21ab2bde0f995d44dba8443c5e2c4c
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-22 13:41:15 -0800

    created

--- random.cs
+++ random.cs
@@ -4,3 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+Random randomNumbers = new Random(); // create random number generator object
+
+// generate 3 random numbers
+int num1 = randomNumbers.Next(1, 6+1);
+int num2 = randomNumbers.Next(1, 6+1);
+int num3 = randomNumbers.Next(1, 6+1);
+
+// Output
+Console.WriteLine("Thrown Numbers: " + num1 + " " + num2 + " " + num3)
