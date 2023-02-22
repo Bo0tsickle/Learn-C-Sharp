0a1b60dcd2246c30bddad642d2c1bd8a854bcf64
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-22 13:19:48 -0800

    borp

--- date-time.cs
+++ date-time.cs
@@ -9,5 +9,5 @@ DateTime today;
 now = DateTime.Now;
 today = DateTime.Today;
 Console.WriteLine("Now is " + now);
-Console.WriteLine("Today is " + today);
+Console.WriteLine("Today is " + today.ToLongDateString());
 Console.ReadLine();
