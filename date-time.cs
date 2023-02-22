4019b0ba90b2883a3756f39ffb52122b1e8f13ae
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-22 13:06:31 -0800

    everything

---
+++ date-time.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+DateTime now;
+now = DateTime.Now;
+Console.WriteLine("Now is " + now);
+Console.ReadLine();
