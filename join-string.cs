3bb45e1d038e14d3bd92cfce0ebfeed57d51fb46
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-22 12:06:10 -0800

    written

---
+++ join-string.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+Console.WriteLine("I have started to program");
+Console.WriteLine(" in C#.");
+Console.WriteLine("I have started to program" + " in C#.");
+Console.ReadLine();
