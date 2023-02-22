63606fef1685ebf8e80ebca2fd5571c5327816b9
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-22 13:08:55 -0800

    today

--- date-time.cs
+++ date-time.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 DateTime now;
+DateTime today;
 now = DateTime.Now;
+today = DateTime.Today;
 Console.WriteLine("Now is " + now);
+Console.WriteLine("Today is " + today);
 Console.ReadLine();
