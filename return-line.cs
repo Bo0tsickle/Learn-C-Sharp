a16a6c5805d6267e468ffff76bef11f1938fc8b9
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-23 12:16:29 -0800

    wrote program

---
+++ return-line.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// get input
+string input = Console.ReadLine();
+
+// output
+Console.WriteLine(input);
