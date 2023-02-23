fc378d4c4a7ed2bbe99a6fedf3149a269beb6ab8
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-23 13:38:57 -0800

    try/catch

--- addition.cs
+++ addition.cs
@@ -4,15 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// take input
-Console.Write("First number: ");
-string input1 = Console.ReadLine();
-Console.Write("Second Number: ");
-string input2 = Console.ReadLine();
-
-// convert input
-int num1 = Convert.ToInt32(input1);
-int num2 = Convert.ToInt32(input2);
-
-// give output
-Console.WriteLine("Your result is: " + (num1 + num2));
+try {
+    // take input
+    Console.Write("First number: ");
+    string input1 = Console.ReadLine();
+    Console.Write("Second Number: ");
+    string input2 = Console.ReadLine();
+    
+    // convert input
+    int num1 = Convert.ToInt32(input1);
+    int num2 = Convert.ToInt32(input2);
+    
+    // give output
+    Console.WriteLine("Your result is: " + (num1 + num2));
+}
+catch (System.Exception) {
+    Console.WriteLine("Incorrect input - cannot calculate");
+}
