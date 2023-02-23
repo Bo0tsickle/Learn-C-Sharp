8e3a39b973ec14f6ff6e1518fa9dbed698e49f56
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-23 13:43:24 -0800

    first input

---
+++ two-calculator.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// input and conversion
+Console.Write("First number: ");
+string num = Console.ReadLine();
