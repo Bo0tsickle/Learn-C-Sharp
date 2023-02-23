2579777742547cb1058950474cbbc9b84a9b3d4b
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-23 12:34:57 -0800

    d

---
+++ addition.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// take input
+Console.Write("First number: ");
+string input1 = Console.ReadLine();
+Console.Write("Second Number: ");
+string input2 = Console.ReadLine();
+
+// convert input
+int num1 = Convert.ToInt32(input1);
+int num2 = Convert.ToInt32(input2);
+
+// give output
+Console.WriteLine("Your result is: " + (num1 + num2));
