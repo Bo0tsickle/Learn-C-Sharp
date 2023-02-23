00bacb6a64c2391a9553daefded3ac8ad5f58815
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-23 13:45:18 -0800

    num2

--- two-calculator.cs
+++ two-calculator.cs
@@ -7,3 +7,8 @@ using System.Threading.Tasks;
 // input and conversion
 Console.Write("First number: ");
 string num = Console.ReadLine();
+double num1 = Convert.ToInt32(num);
+
+Console.Write("Second number: ");
+num = Console.ReadLine();
+double num2 = Convert.ToInt32(num);
