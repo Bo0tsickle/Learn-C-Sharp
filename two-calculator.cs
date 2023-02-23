df64f45079ea4d9c8e9d4356f5e98b861d8d2cd1
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-23 13:50:23 -0800

    refactored

--- two-calculator.cs
+++ two-calculator.cs
@@ -4,17 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// input and conversion
-Console.Write("First number: ");
-string num = Console.ReadLine();
-double num1 = Convert.ToDouble(num);
-
-Console.Write("Second number: ");
-num = Console.ReadLine();
-double num2 = Convert.ToDouble(num);
-
-// output and calculations
-Console.WriteLine("Sum is " + (num1 + num2));
-Console.WriteLine("Difference is " + (num1 - num2));
-Console.WriteLine("Product is " + (num1 * num2));
-Console.WriteLine("Quotient is " + (num1 / num2));
+try {
+    // input and conversion
+    Console.Write("First number: ");
+    string num = Console.ReadLine();
+    double num1 = Convert.ToDouble(num);
+    
+    Console.Write("Second number: ");
+    num = Console.ReadLine();
+    double num2 = Convert.ToDouble(num);
+    
+    // output and calculations
+    Console.WriteLine("Sum is " + (num1 + num2));
+    Console.WriteLine("Difference is " + (num1 - num2));
+    Console.WriteLine("Product is " + (num1 * num2));
+    Console.WriteLine("Quotient is " + (num1 / num2));
+}
+catch (System.Exception) {
+    Console.WriteLine("Incorrect Input");
+}
