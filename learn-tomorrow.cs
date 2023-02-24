5220b0191b1300926a9ffe54a8b1bd72b2fbab8b
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-24 11:47:41 -0800

    bad solution

---
+++ learn-tomorrow.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Output
+Console.WriteLine("I will start learning tomorrow.");
+Console.WriteLine("I will start learning tomorrow.");
+Console.WriteLine("I will start learning tomorrow.");    
+Console.WriteLine("I will start learning tomorrow.");
+Console.WriteLine("I will start learning tomorrow.");
+
+Console.WriteLine("I will start learning tomorrow.");
+Console.WriteLine("I will start learning tomorrow.");
+Console.WriteLine("I will start learning tomorrow.");
+Console.WriteLine("I will start learning tomorrow.");
+Console.WriteLine("I will start learning tomorrow.");
