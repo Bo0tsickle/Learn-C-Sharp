40ee4e86c035f7eb281e1949752917c99b2e4c76
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-24 11:10:49 -0800

    wow

---
+++ chapter-3/source/vat-invoice.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+try
+{
+    // get input
+    Console.Write("Payment date: ");
+    DateTime paymentDate = Convert.ToDateTime(Console.ReadLine());
+    Console.Write("Invoice date: ");
+    DateTime invoiceDate = Convert.ToDateTime(Console.ReadLine());
+    
+    // do checks to see if the invoice date is before the payment date
+    bool legality = true;
+    if (invoiceDate < paymentDate) {
+        Console.WriteLine("Invoice cannot precede payment.");
+        legality = false;
+    }
+    if (invoiceDate > paymentDate.AddDays(15)) {
+        Console.WriteLine("Invoice cannot be over 15 days after payment.");
+        legality = false;
+    }
+    
+    // output
+    if (legality) {
+        Console.WriteLine("Dates accepted.");
+    }
+}
+catch (System.Exception)
+{
+    Console.WriteLine("Invalid Input");
+}
