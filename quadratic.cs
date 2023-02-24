88157e31e4a576548222effab412bdb9aa519682
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-24 11:32:54 -0800

    bug fix

--- quadratic.cs
+++ quadratic.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 // get input
 Console.Write("Enter a: ");
-int a = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToDouble(Console.ReadLine());
 Console.Write("Enter b: ");
-int b = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToDouble(Console.ReadLine());
 Console.Write("Enter c: ");
-int c = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToDouble(Console.ReadLine());
 
 // first, calculate the discriminant
-int D = (b ^ 2) - (4 * a * c);
+double D = (b * b) - (4 * a * c);
 
 // then, branch based on the discriminant
 if (D > 0) {
-    int solution1 = (-b + D) / (2 * a);
-    int solution2 = (-b - D) / (2 * a);
+    int solution1 = (-b + Math.Sqrt(D)) / (2 * a);
+    int solution2 = (-b - Math.Sqrt(D)) / (2 * a);
     Console.WriteLine("The first solution is " + solution1 + " and the second solution is " + solution2 + ".");
 }
 if (D == 0) {
