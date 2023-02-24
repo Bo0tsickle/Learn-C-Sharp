04134b7cec96a181e54b1df50b96cb95d9120061
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-23 18:18:24 -0800

    d

--- commision.cs
+++ commision.cs
@@ -4,4 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-Console.WriteLine()
+// get product price input
+Console.Write("Product Price: ");
+double Product = Console.ReadLine();
+
+// commisions input
+Console.Write("Merchant Commission (percent): ");
+int Merchant = Console.ReadLine();
+
+Console.Write("Distributor Commission (percent)");
+int Distributor = Console.ReadLine();
