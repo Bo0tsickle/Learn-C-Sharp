cbe718930bdb1f07473da1772dab130315536c5c
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-24 19:37:52 -0800

    changed

--- moon-landing.cs
+++ moon-landing.cs
@@ -11,24 +11,37 @@ double acceleration = 0.0;
 double fuel = 35.0;
 double brakePercent = 0.0;
 double brakeForce = 0.0;
+bool malfunction = false;
 
 // announcement
 Console.WriteLine("- - - MOON LANDING - - -");
 Console.WriteLine(" ");
 
 // begin loop
-while (height > 0) {
-    Console.WriteLine("Height: " + height + "  Velocity: " + velocity + "  Fuel: " + fuel); // step 1: announce variables
+while (height >= 0) {
+    Console.WriteLine("Height: " + height.ToString("N1") + "  Velocity: " + velocity.ToString("N1") + "  Fuel: " + fuel.ToString("N1")); // step 1: announce variables
     Console.Write("Enter percentage of braking (0 to 100): "); // step 2: enter and recieve braking percentage
-    brakePercent = (Convert.ToInt16(Console.ReadLine())) / 100; // and remember to convert it to a decimal
+    brakePercent = (Convert.ToDouble(Console.ReadLine())) / 100; // and remember to convert it to a decimal
+    try {
+        if (brakePercent < 0 || brakePercent > 100) {
+            malfunction = true;
+        }
+    }
+    catch (Exception) {
+        malfunction = true;
+    }
+    if (malfunction) {
+        Console.WriteLine("CONTROL MALFUNCTION! CONTROL MALFUNCTION!");
+        brakePercent = 0;
+    }
     Console.WriteLine(" ");
 
     // step 3. calculations for everything
     brakeForce = 360 * brakePercent; 
-    fuel = fuel - ((-brakeForce) / 3000);
+    fuel -= -brakeForce / 3000;
     acceleration = 1.62 - (brakeForce / 8000);
-    velocity = velocity + acceleration;
-    height = height - ((-velocity) - (acceleration / 2));
+    velocity += acceleration;
+    height -= velocity + acceleration / 2;
 }
 
 // ending output
