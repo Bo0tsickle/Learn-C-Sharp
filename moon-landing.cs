fa52080ff4208e79a922609ba9d0aec51721c7df
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-24 19:23:25 -0800

    deez

--- moon-landing.cs
+++ moon-landing.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 // set initial values
 double height = 50.0;
 double velocity = 8.0;
+double acceleration = 0.0;
 double fuel = 35.0;
 double brakePercent = 0.0;
 double brakeForce = 0.0;
@@ -18,14 +19,16 @@ Console.WriteLine(" ");
 // begin loop
 while (height > 0) {
     Console.WriteLine("Height: " + height + "  Velocity: " + velocity + "  Fuel: " + fuel); // step 1: announce variables
-    Console.WriteLine("Enter percentage of braking (0 to 100): "); // step 2: enter and recieve braking percentage
+    Console.Write("Enter percentage of braking (0 to 100): "); // step 2: enter and recieve braking percentage
     brakePercent = (Convert.ToInt16(Console.ReadLine())) / 100; // and remember to convert it to a decimal
+    Console.WriteLine(" ");
 
     // step 3. calculations for everything
     brakeForce = 360 * brakePercent; 
     fuel = fuel - ((-brakeForce) / 3000);
-    velocity = velocity + (1.62 - (brakeForce / 8000));
-    height = height - ((-velocity) - (velocity / 2));
+    acceleration = 1.62 - (brakeForce / 8000);
+    velocity = velocity + acceleration;
+    height = height - ((-velocity) - (acceleration / 2));
 }
 
 // ending output
