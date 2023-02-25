e226d53c9cf646c15a41d8461587d58a88bd30fd
Bo0tsickle <zelda.geek616@gmail.com>
2023-02-24 18:47:06 -0800

    string to int type conversion bug fix

--- power.cs
+++ power.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 // get input
 Console.Write("Enter x (number to be raised): ");
-int numberToBeRaised = Console.ReadLine();
+int numberToBeRaised = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter n (power): ");
-int power = Console.ReadLine();
+int power = Convert.ToInt32(Console.ReadLine());
 
 // do loop calculations
 for (power; power <= 1; power--) {
