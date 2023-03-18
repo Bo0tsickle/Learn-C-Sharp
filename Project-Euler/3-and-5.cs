fa5ec6d098df873456f607cc265e044dcd570085
Bo0tsickle <zelda.geek616@gmail.com>
2023-03-18 16:38:34 -0700

    so good

---
+++ Project-Euler/3-and-5.cs
@@ -0,0 +1,16 @@
+// define variables
+int[] numbers;
+
+// do calculations
+for (int i = 0; i < 1000;) {
+    if ((i % 3 == 0) || (i % 5 == 0)) { numbers.Add(i); }
+}
+
+int sum = 0;
+foreach (int num in numbers) {
+    sum += num;
+}
+
+// output
+Console.Write("Answer: ");
+Console.WriteLine(sum);
