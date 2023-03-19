c616a29219d795ce9d43124d77dcfb388d9ded23
Bo0tsickle <zelda.geek616@gmail.com>
2023-03-18 17:20:44 -0700

    the simplest solution, not the most complex

--- Project-Euler/3-and-5.cs
+++ Project-Euler/3-and-5.cs
@@ -1,13 +1,8 @@
 using System;
 
-int[] numbers = new int[];
-for (int i = 0; i < 1000;) {
-    if ((i % 3 == 0) || (i % 5 == 0)) { numbers.Append(i).ToArray(); }
-}
-
 int sum = 0;
-foreach (int num in numbers) {
-    sum += num;
+for (int i = 0; i < 1000;) {
+    if ((i % 3 == 0) || (i % 5 == 0)) { sum += i; }
 }
 
 // output
