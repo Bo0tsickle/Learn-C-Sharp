6fd0b9a2bf3858b89d08c40d2da7332bd423cf89
Bo0tsickle <zelda.geek616@gmail.com>
2023-03-24 18:16:40 -0700

    stuff

--- advent-of-code/2015/d1/puzzle.cs
+++ advent-of-code/2015/d1/puzzle.cs
@@ -1,6 +1,17 @@
 using System;
 using System.IO;
 
+int floor = 0;
 string inputPath = "input"; 
 string input = File.ReadAllText(inputPath);
 
+
+foreach (var parenthese in input) {
+    switch(parenthese) {
+        case '(': floor++;
+        case ')': floor--;
+        default: Console.WriteLine("this should not be happening  w h a t");
+    }
+}
+
+Console.WriteLine(floor);
