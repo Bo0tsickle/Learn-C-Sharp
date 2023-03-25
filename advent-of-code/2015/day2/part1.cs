f63a74348c5c89b788fa197106692a2e83b509dd
Bo0tsickle <zelda.geek616@gmail.com>
2023-03-24 22:01:49 -0700

    made the GetDimensionsFromStrInput() method, let's see if it works

--- advent-of-code/2015/day2/part1.cs
+++ advent-of-code/2015/day2/part1.cs
@@ -5,3 +5,17 @@ string[] input = File.ReadAllLines("input.txt");
 for(int i = 0; i < input.Length; i++) {
     string tempInput = input[i];
 }
+
+int[] GetDimensionsFromStrInput(string input) {
+    int[] dimensions = new int[3];
+    string strDimension = "";
+    for (int i = 0; i < 3; i++) {
+       if (input[i] == 'x') {
+            dimensions[i] = strDimension;
+       } 
+       else {
+            strDimension += input[i];
+       }
+    }
+    return dimensions;
+}
